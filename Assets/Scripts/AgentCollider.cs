0e649349cee25eddf35aef8faa990a9fddef254e
fernandordev <fernandorodriguesdev@outlook.com>
2023-04-06 01:08:34 -0300

    feat: removing agents code and add a collider

---
+++ Assets/Scripts/AgentCollider.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AgentCollider : MonoBehaviour
+{
+    public bool drawDebugBox = true;
+    public List<GameObject> objectsInsideCollider = new List<GameObject>();
+    public string tagToDetect = "AutonomousVehicle";
+    private Dictionary<GameObject, float> timesInsideCollider = new Dictionary<GameObject, float>();
+
+    private void OnDrawGizmosSelected()
+    {
+        if (drawDebugBox)
+        {
+            BoxCollider myCollider = GetComponent<BoxCollider>();
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(myCollider.bounds.center, myCollider.bounds.size);
+        }
+
+        foreach (GameObject gameObject in objectsInsideCollider)
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+
+            if (rb == null || rb.velocity.magnitude < 0.01f)
+            {
+                Gizmos.color = Color.red;
+            }
+            else
+            {
+                Gizmos.color = Color.green;
+            }
+
+            Gizmos.DrawSphere(new Vector3(gameObject.transform.position.x, 1f, gameObject.transform.position.z), 0.1f);
+
+            if (timesInsideCollider.ContainsKey(gameObject))
+            {
+                float timeInside = Time.time - timesInsideCollider[gameObject];
+                string timeString = timeInside.ToString("F1");
+                Vector3 textPosition = new Vector3(gameObject.transform.position.x, 1.5f, gameObject.transform.position.z);
+                GUIStyle style = new GUIStyle();
+                style.alignment = TextAnchor.MiddleCenter;
+                style.normal.textColor = Color.black;
+                Handles.Label(textPosition, timeString, style);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagToDetect))
+        {
+            objectsInsideCollider.Add(other.gameObject);
+            timesInsideCollider[other.gameObject] = Time.time;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagToDetect))
+        {
+            objectsInsideCollider.Remove(other.gameObject);
+            float timeEntered = timesInsideCollider[other.gameObject];
+            float timeInside = Time.time - timeEntered;
+            Debug.Log(other.gameObject.name + " was inside the collider for " + timeInside + " seconds");
+            timesInsideCollider.Remove(other.gameObject);
+        }
+    }
+
+    public void PrintTimesInsideCollider()
+    {
+        foreach (KeyValuePair<GameObject, float> pair in timesInsideCollider)
+        {
+            Debug.Log(pair.Key.name + " entered the collider at time " + pair.Value);
+        }
+    }
+}
--- Assets/Scripts/IntersectionAgent.cs
+++ Assets/Scripts/IntersectionAgent.cs
@@ -8,16 +8,7 @@ using TrafficSimulation;
 public class IntersectionAgent : Agent
 {
     public Intersection intersection;
-    private int actionIndex = 0;
-    private float totalWaitTime = 0.0f;
-    private float maxWaitTime = 60.0f; // limite de tempo de espera antes de terminar o episódio
-    private Dictionary<GameObject, float> vehicleArrivalTimes = new Dictionary<GameObject, float>(); // dicionário para armazenar os tempos de chegada dos veículos
-
-
     // variáveis para controle do tempo de troca de ações
-    private float lastActionTime = 0.0f;
-    public float minActionInterval = 8.0f; // tempo mínimo entre trocas de ações
-
     void Start()
     {
         Debug.Log("Start called!");
@@ -26,28 +17,12 @@ public class IntersectionAgent : Agent
     public override void OnEpisodeBegin()
     {
         Debug.Log("Episode begin!");
-        totalWaitTime = 0.0f;
-        vehicleArrivalTimes.Clear(); // limpa o dicionário de tempos de chegada dos veículos
     }
-
-    // public override void CollectObservations(VectorSensor sensor)
-    // {
-    //     sensor.AddObservation(intersection.vehiclesQueue.Count);
-    // }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         int action = actionBuffers.DiscreteActions[0];
 
-        // check if enough time has passed since last action switch
-        if (Time.time - lastActionTime < minActionInterval)
-        {
-            // not enough time has passed, do not switch the lights
-            SetReward(-0.1f);
-            return;
-        }
-
-        // enough time has passed, switch the lights
         if (action == 0)
         {
             intersection.SwitchLightsToGroup1();
@@ -57,54 +32,20 @@ public class IntersectionAgent : Agent
             intersection.SwitchLightsToGroup2();
         }
 
-        // update the timer
-        lastActionTime = Time.time;
-        // Calcula a recompensa baseada no tempo de espera dos veículos na fila
-        float reward = 0.0f;
-        foreach (GameObject vehicle in intersection.vehiclesQueue)
-        {
-            float waitingTime = 0.0f;
-            if (!vehicleArrivalTimes.TryGetValue(vehicle, out waitingTime)) // se o veículo não está no dicionário, adiciona-o com o tempo de chegada atual
-            {
-                vehicleArrivalTimes.Add(vehicle, Time.time);
-            }
-            else // se o veículo já está no dicionário, calcula o tempo de espera atual
-            {
-                waitingTime = Time.time - waitingTime;
-            }
-
-            if (waitingTime > maxWaitTime)
-            {
-                SetReward(-0.01f);
-                EndEpisode(); // Termina o episódio se um veículo esperou por mais tempo do que o permitido
-                return;
-            }
-            else if (waitingTime > 0.0f)
-            {
-                reward += 1.0f / waitingTime; // Adiciona uma recompensa proporcional ao inverso do tempo de espera
-            }
-        }
-        SetReward(reward);
-
-        totalWaitTime += Time.deltaTime;
-
-        // Termina o episódio se o tempo de espera máximo for ultrapassado
-        if (totalWaitTime > maxWaitTime)
-        {
-            SetReward(-0.01f);
-            EndEpisode();
-        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        Debug.Log("Heuristic called!");
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            actionIndex = (actionIndex == 0) ? 1 : 0; // Alterna entre ação 0 e 1
-        }
+        var discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = -1;
 
-        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = actionIndex;
+        if (Input.GetKey(KeyCode.A))
+        {
+            discreteActions[0] = 0;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            discreteActions[0] = 1;
+        }
     }
 }
